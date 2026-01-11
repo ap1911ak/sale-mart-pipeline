2bf057b8dfcef66fdb000ff0ffa2255d6452cb60
CrocodileSoy <ap1911ak@gmail.com>
2026-01-11 11:42:50 +0700

    edit extract.py line 8 and run

--- extract.py
+++ extract.py
@@ -5,13 +5,14 @@ def extract():
     source_dir = 'Landing_zone'
     dest_dir = 'Raw_zone'
 
-    if not os.path.exists(_dir):
+    if not os.path.exists(dest_dir):
         os.makedirs(dest_dir)
 
-    files = [f  for f in os.listdir(source_dir) if f.endswith('.csv')]
+    files = [f for f in os.listdir(source_dir) if f.endswith('.csv')]
 
     for file in files:
         shutil.copy(os.path.join(source_dir, file), os.path.join(dest_dir, file))
         print(f"Extracted files from {source_dir} to {dest_dir} {len(files)},files : {files} ")
+
 if __name__ == "__main__":
-    extract()    
+    extract()
